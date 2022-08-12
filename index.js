6868b64a75b72f3ae1016777266b5c9f497b21ce
Nuno980627 <952571278@qq.com>
2022-08-12 12:58:19 +0800

    新增react模板

--- index.js
+++ index.js
@@ -9,12 +9,35 @@ import path from 'path'
 import {fsExist} from './util.js'
 import { promisify } from 'util'
 import fs from 'fs'
+const promptList = [{
+  type: 'list',
+  message: 'select a code template:',
+  name: 'templateUrl',
+  choices: [
+      "vite vue3",
+      "vite react18"
+  ],
+  filter: function (val) { // 使用filter将回答变为小写
+    let url=''
+    switch (val) {
+      case  "vite vue3":
+        url ='https://github.com/Nuno980627/vite-demo-vant.git#main'
+        break;
+      case  "vite react18":
+        url ='https://github.com/Nuno980627/antd-react-mobile.git#main'
+        break;
+      default:
+        break;
+    }
+    return url
+  }
+}];
 // set version
-program.version("1.0.0");
+program.version("1.1.0");
 
 program
   .command("create <name>") //<>必填参数 []选填参数
-  .description("创建一个 vite-vue3-ts-vant 项目模板")
+  .description("创建一个 vue react 项目模板")
   .action(async(name) => {
     const targetPath=path.resolve(process.cwd(),name)
     console.log(chalk.bold.rgb(147, 212, 117)('[vmobile] target path:'+targetPath))
@@ -24,12 +47,14 @@ program
       console.log(chalk.red('[vmobile] 该文件已存在，尝试更改项目名称'));
       process.exit(1);
     }
-    const spinner=ora({
+    inquirer.prompt(promptList).then(answers => {
+      const {templateUrl}=answers
+      const spinner=ora({
       text:'正在生成...',
       color:'blue'
     }).start()
     download(
-      "direct:https://github.com/Nuno980627/vite-demo-vant.git#main",
+      `direct:${templateUrl}`,
       name,
       { clone: true },
       function (err) {
@@ -41,11 +66,13 @@ program
         const filePath=path.join(targetPath,'./package.json')
         const data = fs.readFileSync(filePath)
         let packagesJSON=data.toString()
-        packagesJSON= packagesJSON.replace(/vite-vue3-ts-vant/g, name.trim())
+        packagesJSON= packagesJSON.replace(/code-template/g, name.trim())
         fs.writeFileSync(filePath,packagesJSON)
         spinner.succeed('[vmobile] generate template complete')
         process.exit(0);
       }
     );
+    })
+
   });
 program.parse(process.argv);
